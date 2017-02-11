c51f968e9903571f014e429a605a80cdd542e0b3
Navakanth <navakanthtunga@gmail.com>
2017-02-11 18:57:24 +0530

    Add files via upload

---
+++ sample/src/test/java/com/testscripts/FindBrokenLinks.java
@@ -0,0 +1,73 @@
+package com.others;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.liqvid.library.GenericLib;
+public class FindBrokenLinks {
+
+
+static WebDriver driver;
+
+		public static void main(String[] args) 
+		{
+
+			System.setProperty("webdriver.gecko.driver",GenericLib.sDirPath+"\\resources\\geckodriver.exe");
+			System.out.println("Firefox Browser is set");
+			driver = new FirefoxDriver()	;		
+			driver.manage().window().maximize();
+			
+			driver.get("http://www.google.co.in/");
+			
+			List<WebElement> links=driver.findElements(By.tagName("a"));
+			
+			System.out.println("Total links are "+links.size());
+			
+			for(int i=0;i<links.size();i++)
+			{
+				
+				WebElement ele= links.get(i);
+				
+				String url=ele.getAttribute("href");
+				
+				verifyLinkActive(url);
+				
+			}
+			
+		}
+		
+		public static void verifyLinkActive(String linkUrl)
+		{
+	        try 
+	        {
+	           URL url = new URL(linkUrl);
+	           
+	           HttpURLConnection httpURLConnect=(HttpURLConnection)url.openConnection();
+	           
+	           httpURLConnect.setConnectTimeout(3000);
+	           
+	           httpURLConnect.connect();
+	           
+	           if(httpURLConnect.getResponseCode()==200)
+	           {
+	               System.out.println(linkUrl+" - "+httpURLConnect.getResponseMessage());
+	            }
+	          if(httpURLConnect.getResponseCode()==HttpURLConnection.HTTP_NOT_FOUND)  
+	           {
+	               System.out.println(linkUrl+" - "+httpURLConnect.getResponseMessage() + " - "+ HttpURLConnection.HTTP_NOT_FOUND);
+	            }
+	        } catch (Exception e) {
+	           
+	        } 
+	    } 
+		
+		
+		 
+
+	
+}
